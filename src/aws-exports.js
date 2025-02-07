227881e5be35b4c705cbd1fbad4d797392a8a866
ThompyTime <christopherjohnthompson_9@outlook.com>
2025-02-07 19:44:57 +0000

    Updated UserPoolWebClientId

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,10 @@
 const awsconfig = {
-    Auth: {
-      identityPoolId: 'eu-west-2:e92d684c-105c-4da0-951c-caea1078eba6',
-      region: 'eu-west-2',
-      userPoolId: 'eu-west-2_LzxVNMylk',
-      userPoolWebClientId: '3nh4u7ldblp30i25d0kdc6sq5f',
-    },
-  };
-  
+  Auth: {
+      identityPoolId: "eu-west-2:e92d684c-105c-4da0-951c-caea1078eba6", 
+      region: "eu-west-2",
+      userPoolId: "eu-west-2_LzxVNMylk",
+      userPoolWebClientId: "6m9g6m0s84on19m80225hefuhc",
+  },
+};
+
+export default awsconfig;
