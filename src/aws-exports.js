9070266250f360bd8df43487562cea3743c0151f
ThompyTime <christopherjohnthompson_9@outlook.com>
2025-02-07 19:49:45 +0000

    Updated aws-exports.js with correct App Client ID

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@ const awsconfig = {
       identityPoolId: "eu-west-2:e92d684c-105c-4da0-951c-caea1078eba6", 
       region: "eu-west-2",
       userPoolId: "eu-west-2_LzxVNMylk",
-      userPoolWebClientId: "6m9g6m0s84on19m80225hefuhc",
+      userPoolWebClientId: "3nh4u7ldblp30i25d0kdc6sq5f",
   },
 };
 
