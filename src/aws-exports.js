d4386e8e3e88d4c51d9b6d80b8a2840dd061ebd6
ThompyTime <christopherjohnthompson_9@outlook.com>
2025-02-08 08:27:39 +0000

    Updated aws-exports.js with latest config

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,7 +1,6 @@
 const awsconfig = {
   Auth: {
       identityPoolId: "eu-west-2:e92d684c-105c-4da0-951c-caea1078eba6", 
-      region: "eu-west-2",
       userPoolId: "eu-west-2_LzxVNMylk",
       userPoolWebClientId: "3nh4u7ldblp30i25d0kdc6sq5f",
   },
